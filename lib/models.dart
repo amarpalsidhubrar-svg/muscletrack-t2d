7c4624d36322ef57a0d6f826a3ce2a0e5c26a6af
amarpalsidhubrar-svg <amarpalsidhubrar@gmail.com>
2026-09-20 17:05:12 +1000

    Simplify models to workout-only data

--- lib/models.dart
+++ lib/models.dart
@@ -1,101 +1,15 @@
 class UserProfile {
   final String name;
-  final int age;
-  final String sex;
-  final double heightCm;
-  final double baselineWeightKg;
 
-  const UserProfile({
-    required this.name,
-    required this.age,
-    required this.sex,
-    required this.heightCm,
-    required this.baselineWeightKg,
-  });
+  const UserProfile({required this.name});
 
   Map<String, Object?> toMap() => {
         'id': 1,
         'name': name,
-        'age': age,
-        'sex': sex,
-        'height_cm': heightCm,
-        'baseline_weight_kg': baselineWeightKg,
       };
 
   factory UserProfile.fromMap(Map<String, Object?> map) => UserProfile(
         name: (map['name'] as String?) ?? '',
-        age: (map['age'] as num).toInt(),
-        sex: map['sex'] as String,
-        heightCm: (map['height_cm'] as num).toDouble(),
-        baselineWeightKg: (map['baseline_weight_kg'] as num).toDouble(),
-      );
-}
-
-class WeightEntry {
-  final int? id;
-  final DateTime date;
-  final double weightKg;
-  final double? bodyFatPct;
-  final double? leanMassKg;
-
-  const WeightEntry({
-    this.id,
-    required this.date,
-    required this.weightKg,
-    this.bodyFatPct,
-    this.leanMassKg,
-  });
-
-  Map<String, Object?> toMap() => {
-        if (id != null) 'id': id,
-        'date': date.toIso8601String(),
-        'weight_kg': weightKg,
-        'body_fat_pct': bodyFatPct,
-        'lean_mass_kg': leanMassKg,
-      };
-
-  factory WeightEntry.fromMap(Map<String, Object?> map) => WeightEntry(
-        id: map['id'] as int?,
-        date: DateTime.parse(map['date'] as String),
-        weightKg: (map['weight_kg'] as num).toDouble(),
-        bodyFatPct: (map['body_fat_pct'] as num?)?.toDouble(),
-        leanMassKg: (map['lean_mass_kg'] as num?)?.toDouble(),
-      );
-}
-
-class MedicationEntry {
-  final int? id;
-  final DateTime date;
-  final String medicationName;
-  final String medicationClass;
-  final String dose;
-  final String note;
-
-  const MedicationEntry({
-    this.id,
-    required this.date,
-    required this.medicationName,
-    required this.medicationClass,
-    required this.dose,
-    this.note = '',
-  });
-
-  Map<String, Object?> toMap() => {
-        if (id != null) 'id': id,
-        'date': date.toIso8601String(),
-        'medication_name': medicationName,
-        'medication_class': medicationClass,
-        'dose': dose,
-        'note': note,
-      };
-
-  factory MedicationEntry.fromMap(Map<String, Object?> map) => MedicationEntry(
-        id: map['id'] as int?,
-        date: DateTime.parse(map['date'] as String),
-        medicationName: map['medication_name'] as String,
-        medicationClass: map['medication_class'] as String,
-        dose: map['dose'] as String,
-        note: (map['note'] as String?) ?? '',
       );
 }
 
@@ -142,7 +56,6 @@ class WorkoutSession {
   final int durationMin;
   final double? met;
   final String source;
-  final double? deviceCalories;
   final int? fatigue;
   final int? sleepQuality;
   final int? muscleSoreness;
@@ -160,7 +73,6 @@ class WorkoutSession {
     required this.durationMin,
     this.met,
     this.source = 'Manual entry',
-    this.deviceCalories,
     this.fatigue,
     this.sleepQuality,
     this.muscleSoreness,
@@ -179,7 +91,6 @@ class WorkoutSession {
         'duration_min': durationMin,
         'met': met,
         'source': source,
-        'device_calories': deviceCalories,
         'fatigue': fatigue,
         'sleep_quality': sleepQuality,
         'muscle_soreness': muscleSoreness,
@@ -189,88 +100,6 @@ class WorkoutSession {
         'session_rpe': sessionRpe,
         'notes': notes,
       };
-}
-
-class MealEntry {
-  final int? id;
-  final DateTime date;
-  final String mealType;
-  final String description;
-  final double calories;
-  final double? proteinG;
-  final double? carbsG;
-  final double? fatG;
-  final double? fibreG;
-
-  const MealEntry({
-    this.id,
-    required this.date,
-    required this.mealType,
-    required this.description,
-    required this.calories,
-    this.proteinG,
-    this.carbsG,
-    this.fatG,
-    this.fibreG,
-  });
-
-  Map<String, Object?> toMap() => {
-        if (id != null) 'id': id,
-        'date': date.toIso8601String(),
-        'meal_type': mealType,
-        'description': description,
-        'calories': calories,
-        'protein_g': proteinG,
-        'carbs_g': carbsG,
-        'fat_g': fatG,
-        'fibre_g': fibreG,
-      };
-
-  factory MealEntry.fromMap(Map<String, Object?> map) => MealEntry(
-        id: map['id'] as int?,
-        date: DateTime.parse(map['date'] as String),
-        mealType: map['meal_type'] as String,
-        description: map['description'] as String,
-        calories: (map['calories'] as num).toDouble(),
-        proteinG: (map['protein_g'] as num?)?.toDouble(),
-        carbsG: (map['carbs_g'] as num?)?.toDouble(),
-        fatG: (map['fat_g'] as num?)?.toDouble(),
-        fibreG: (map['fibre_g'] as num?)?.toDouble(),
-      );
-}
-
-class Goals {
-  final double? targetWeightKg;
-  final int weeklyActivityMin;
-  final int weeklyStrengthSessions;
-  final String targetExercise;
-  final double? targetE1rmKg;
-
-  const Goals({
-    this.targetWeightKg,
-    this.weeklyActivityMin = 150,
-    this.weeklyStrengthSessions = 2,
-    this.targetExercise = '',
-    this.targetE1rmKg,
-  });
-
-  Map<String, Object?> toMap() => {
-        'id': 1,
-        'target_weight_kg': targetWeightKg,
-        'weekly_activity_min': weeklyActivityMin,
-        'weekly_strength_sessions': weeklyStrengthSessions,
-        'target_exercise': targetExercise,
-        'target_e1rm_kg': targetE1rmKg,
-      };
-
-  factory Goals.fromMap(Map<String, Object?> map) => Goals(
-        targetWeightKg: (map['target_weight_kg'] as num?)?.toDouble(),
-        weeklyActivityMin: (map['weekly_activity_min'] as num?)?.toInt() ?? 150,
-        weeklyStrengthSessions:
-            (map['weekly_strength_sessions'] as num?)?.toInt() ?? 2,
-        targetExercise: (map['target_exercise'] as String?) ?? '',
-        targetE1rmKg: (map['target_e1rm_kg'] as num?)?.toDouble(),
-      );
 }
 
 class StrengthBest {
